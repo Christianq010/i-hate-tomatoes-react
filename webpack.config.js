387703576a68eeccfebaaa0d79376f99ed5f2f53
Christiaan Quyn <christianq010@gmail.com>
2018-01-22 19:25:08 +0530

    Feat: Lesson 2 - HTML Webpack Plugin, minify and render via webpack
    Run `npm run dev` to build the index.html file via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: './dist/app.bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            // minify: {
+            //     collapseWhitespace: true
+            // },
+            hash: true,
+            template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+        })
+    ]
 }
