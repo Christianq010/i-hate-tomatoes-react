b5f756f8400da118d8de176478941e4c9911c103
Christiaan Quyn <christianq010@gmail.com>
2018-01-25 13:38:48 +0530

    Lesson 04 - React State and es6 Classes, rendering elements in an array of objects

--- src/app.js
+++ src/app.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Headline = () => {
-    return (
-        <h1 className="some-shit">Hello World!</h1>
-    )
+export class App extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            buyitems: [
+                {
+                    name: "milk",
+                    id: "234"
+                },
+                {
+                    name: "bread",
+                    id: "4545"
+                },
+                {
+                    name: "butter",
+                    id: "5758"
+                }
+            ]
+        }
+    }
+
+
+
+    render() {
+        const {buyitems} = this.state;
+        return (
+            <div className="some-div">    
+                <h1>Shopping List</h1>
+                {
+                    buyitems.map (item => {
+                        return <p key={item.id}> {item.name} </p>
+                    })
+                }
+            </div>
+        )
+    }
 }
 
-const Greeting = (props) => {
-    const {name, age} = props;
-    return (
-        <p>This is a apparently cool {name} ({age})</p>
-    )
-}
 
-export const App = () => {
-    return (
-    <div className="some-div">    
-        <Headline />
-        <Greeting name="Hello" age={26} />
-    </div>
-    )
-}
 
-Greeting.propTypes = {
-    name: PropTypes.string,
-    age: PropTypes.number.isRequired
-}
